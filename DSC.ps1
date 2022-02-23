9d9500f7c2e702ae3c5c81c8f9a8039c3c6d58aa
Daria Smelova <71319663+dariasmelova@users.noreply.github.com>
2022-02-23 08:56:03 +0300

    Create DSC.ps1

---
+++ DSC.ps1
@@ -0,0 +1,18 @@
+Configuration IIS_and_ASP.NET
+{
+  Node 'localhost'
+  {
+    #Install IIS - Enabled via Windows feature
+    WindowsFeature IIS
+    {
+      Ensure = "Present"
+      Name = "Web-Server"
+    }
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = "Present"
+      Name = "Web-Asp-Net45"
+    }
+  }
+}
